0fa616dadda38cf2af64a58cd4e60d8bce919b50
FoxyChmoxy <almasgaara@mail.ru>
2020-03-18 06:03:17 +0600

    Init

---
+++ c5library/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace c5library.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
